ab6553c051ea33346ae5f9e01d90842d21ad7b53
ositha <osithadayan99@gmail.com>
2025-08-05 18:01:08 +0530

    I Create My Blog App

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Plugin/bootstrap.min.css">
+    <script src="Plugin/jquery.min.js"></script>
+    <script src="Plugin/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+
+    <title>Blog App</title>
+</head>
+<body style="background-color=dark-blue">
+
+<?php include_once('inc/navbar.php') ?>
+
+<div class="container">
+    <div class="row mt-4">
+         <div class="col-md-12" style="margin:85px">
+            <div class="jumbotron ">        
+            <h4 id="jumbo-header">Welcome To The DevTubes</h4>   
+            <h4 id="jumbo-emoji">👾🧑‍💻👾</h4> 
+            </div>
+         </div>
+    </div>
+</div>
+
+</body>
+</html>
